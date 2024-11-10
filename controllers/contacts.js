9122600f943932642c7baabb2307cc4b1c319760
Ganonymous <andrewleetham@gmail.com>
2024-11-09 23:18:36 -0800

    working GET connections

---
+++ controllers/contacts.js
@@ -0,0 +1,25 @@
+const mongoDb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+    const result = await mongoDb.getDatabase().db('Project1').collection('contacts').find();
+    result.toArray().then((contacts) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(contacts);
+    });
+};
+
+const getSingle = async (req, res) => {
+    const idString = String(req.params.id);
+    const contactId = new ObjectId(idString);
+    const result = await mongoDb.getDatabase().db('Project1').collection('contacts').find({_id: contactId});
+    result.toArray().then((contacts) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(contacts[0]);
+    });
+};
+
+module.exports = {
+    getAll,
+    getSingle,
+}
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+const mongoDb = require('./data/database');
+
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => (console.log(`Running on port ${port}`)));
+mongoDb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => (console.log(`Running on port ${port}`)));
+    }
+});
