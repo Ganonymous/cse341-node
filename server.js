681142ec5bda0f602891327248fa09c74f430286
Ganonymous <andrewleetham@gmail.com>
2024-11-16 15:46:17 -0800

    Add CORS

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const mongoDb = require('./data/database');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('./swagger.json');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/', require('./routes'));
 
