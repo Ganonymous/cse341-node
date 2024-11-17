732d29c27312bd06204d98aa9f6824be8f511bc2
Ganonymous <andrewleetham@gmail.com>
2024-11-16 19:42:38 -0800

    move swagger doc code to the right place

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
 
 router.get('/', (req, res) => res.send('Hello World'));
-
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const app = express();
 
 const mongoDb = require('./data/database');
 const bodyParser = require('body-parser');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('./swagger.json');
 const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/', require('./routes'));
 
 mongoDb.initDb((err) => {
